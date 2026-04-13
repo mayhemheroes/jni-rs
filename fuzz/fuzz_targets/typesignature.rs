a7c134d43eea782411ea4f1be55e43781fefcc41
Aziz Rizayev <rizaziz2002@forallsecure.com>
2026-04-13 14:39:18 +0000

    Fix Mayhem workflow: rustlang/rust:nightly, updated fuzz harness API, workspace path

--- fuzz/fuzz_targets/typesignature.rs
+++ fuzz/fuzz_targets/typesignature.rs
@@ -1,6 +1,8 @@
 #![no_main]
+use std::str::FromStr;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &str| {
-    _ = jni::signature::TypeSignature::from_str(data);
+    let _ = jni::signature::JavaType::from_str(data);
+    let _ = jni::signature::RuntimeMethodSignature::from_str(data);
 });
